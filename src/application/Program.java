4d70606efd6576083ef361e5af7e8ca0cfa725fb
Jairo Alberto <jairo.albertto@gmail.com>
2024-12-31 22:45:04 -0300

    Initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import entities.Product;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.00));
+        list.add(new Product("Keyboard", 90.00));
+        list.add(new Product("HD Case", 60.00));
+        list.add(new Product("PlayStation 5", 700.00));
+
+
+
+        list.forEach(System.out::println);
+    }
+}
